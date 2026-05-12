bfe3c35d70ae38f6dcef6c1ad575e687ccf2e4be
EmilyMay34 <142463585+EmilyMay34@users.noreply.github.com>
2026-05-12 17:55:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app) # Esto permite que React se conecte sin errores
 
 # Usa tus credenciales de la Fase 2
 SUPABASE_URL = "https://fodbwpaeodiweaatzmxw.supabase.co" 
-SUPABASE_KEY = "sb_publishable_JK0VaywyUg5Q89xQRZQ6qQ_KD_UxdsH"
+SUPABASE_KEY = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvZGJ3cGFlb2Rpd2VhYXR6bXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0Mjg3MDgsImV4cCI6MjA5NDAwNDcwOH0"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/api/datos', methods=['GET'])
